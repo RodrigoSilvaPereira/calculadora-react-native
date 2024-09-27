b8d40fd40f2ed17c0ff59e0c94d3fd1de0c334e3
RodrigoS <2005rodrigosilva@gmail.com>
2024-09-26 22:30:44 -0300

    feat: Adicionando funcionalidade em operadores

--- calculator/App.js
+++ calculator/App.js
@@ -4,39 +4,74 @@ import { SafeAreaView, StyleSheet, Text, View, TouchableHighlight, TextInput } f
 import Display from './components/display';
 import Btn from './components/botao';
 
+
+
+let estados={
+  valorTela: '',
+  resultado: 0,
+  operado: false,
+  ponto: false,
+}
+
 export default function App() {
   
-  const [operacao, setOperacao] = useState(0)
-  const [resultado, setResultado] = useState(0)
+  const [vtela, setVtela] = useState(estados.valorTela)
+  const [vres, setVres] = useState(estados.resultado)
 
-  const operar=()=>{
-    setResultado(eval(operacao))
+  const addDigito=(d)=>{
+
+    estados.valorTela=estados.valorTela+d
+    setVtela(estados.valorTela)
+    setVres(estados.resultado)
+    estados.operado=false
+  }
+
+  const limparTela=()=>{
+    estados={
+      valorTela: '',
+      resultado: 0,
+      operado: false,
+      ponto: false,
+    }
+    setVtela(estados.valorTela)
+    setVres(estados.resultado)
+  }
+
+  const opera=(o)=>{
+    if(d=='AC'){
+      limparTela()
+      return
+    }
+
+    if(d=='BS'){
+      const vt=vtela.substring(0,(vtela.length-1))
+    }
   }
 
   return (
     <SafeAreaView style={estilos.conteiner}>
       <Text>Calculadora - Rodrigo</Text>
-      <Display valor={0} res={0}/>
+      <Display valor={vtela} res={vres}/>
       <View style={estilos.botoes}>
-        <Btn label="AC" ac aoClicar={()=>{}}></Btn>
-        <Btn label="(" aoClicar={()=>{}}></Btn>
-        <Btn label=")" aoClicar={()=>{}}></Btn>
-        <Btn label="/" operacao aoClicar={()=>{}}></Btn>
-        <Btn label="7" aoClicar={()=>{}}></Btn>
-        <Btn label="8" aoClicar={()=>{}}></Btn>
-        <Btn label="9" aoClicar={()=>{}}></Btn>
-        <Btn label="*" operacao aoClicar={()=>{}}></Btn>
-        <Btn label="4" aoClicar={()=>{}}></Btn>
-        <Btn label="5" aoClicar={()=>{}}></Btn>
-        <Btn label="6" aoClicar={()=>{}}></Btn>
-        <Btn label="-" operacao aoClicar={()=>{}}></Btn>
-        <Btn label="1" aoClicar={()=>{}}></Btn>
-        <Btn label="2" aoClicar={()=>{}}></Btn>
-        <Btn label="3" aoClicar={()=>{}}></Btn>
-        <Btn label="+" operacao aoClicar={()=>{}}></Btn>
-        <Btn label="0" aoClicar={()=>{}}></Btn>
-        <Btn label="." operacao aoClicar={()=>{}}></Btn>
-        <Btn label="<-" bs aoClicar={()=>{}}></Btn>
+        <Btn label="AC" ac aoClicar={()=>{opera('AC')}}></Btn>
+        <Btn label="(" aoClicar={()=>{addDigito('(')}}></Btn>
+        <Btn label=")" aoClicar={()=>{addDigito(')')}}></Btn>
+        <Btn label="/" operacao aoClicar={()=>{addDigito('/')}}></Btn>
+        <Btn label="7" aoClicar={()=>{addDigito('7')}}></Btn>
+        <Btn label="8" aoClicar={()=>{addDigito('8')}}></Btn>
+        <Btn label="9" aoClicar={()=>{addDigito('9')}}></Btn>
+        <Btn label="*" operacao aoClicar={()=>{addDigito('*')}}></Btn>
+        <Btn label="4" aoClicar={()=>{addDigito('4')}}></Btn>
+        <Btn label="5" aoClicar={()=>{addDigito('5')}}></Btn>
+        <Btn label="6" aoClicar={()=>{addDigito('6')}}></Btn>
+        <Btn label="-" operacao aoClicar={()=>{addDigito('-')}}></Btn>
+        <Btn label="1" aoClicar={()=>{addDigito('1')}}></Btn>
+        <Btn label="2" aoClicar={()=>{addDigito('2')}}></Btn>
+        <Btn label="3" aoClicar={()=>{addDigito('3')}}></Btn>
+        <Btn label="+" operacao aoClicar={()=>{addDigito('+')}}></Btn>
+        <Btn label="0" aoClicar={()=>{addDigito('0')}}></Btn>
+        <Btn label="." operacao aoClicar={()=>{addDigito('.')}}></Btn>
+        <Btn label="<-" bs aoClicar={()=>{opera('BS')}}></Btn>
         <Btn label="=" igual aoClicar={()=>{}}></Btn>
       </View>
     </SafeAreaView>
@@ -52,5 +87,5 @@ const estilos = StyleSheet.create({
   botoes:{
     flexDirection: 'row',
     flexWrap: 'wrap'
-  }
+  },
 });
